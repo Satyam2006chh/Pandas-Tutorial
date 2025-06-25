5a5b772818679c4911e772e95c4104d0b6077dce
Satyam2006chh <satyam3396.beai24@chitkara.edu.in>
2025-06-25 23:51:36 +0530

    Added Writing to CSV Functionality

--- Tutorial.py
+++ Tutorial.py
@@ -7,3 +7,20 @@ import numpy as np
 
 df  = pd.read_csv('pandas.csv')
 print(df)
+
+
+# What is basically a dataframe - ?
+
+# A DataFrame is a two-dimensional, size-mutable and potentially heterogeneous tabular data structure with labeled axes (rows and columns).
+
+
+# Now to write the data in the CSV -
+
+nme = ["aparna", "pankaj", "sudhir", "Geeku"]
+deg = ["MBA", "BCA", "M.Tech", "MBA"]
+scr = [90, 40, 80, 98]
+
+dict = {"Name":nme , "Degree":deg, "Score":scr}
+df.to_csv("file.csv")
+
+# It will automatically create A csv to your folder
